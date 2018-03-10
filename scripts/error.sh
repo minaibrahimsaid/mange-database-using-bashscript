ab0647d22aaa1568bae2b013940a23de327deab8
F-Yousri <fahdyousri@gmail.com>
2018-03-10 17:46:54 +0200

    added features insert, sort and Alter table

--- scripts/error.sh
+++ scripts/error.sh
@@ -2,6 +2,15 @@
 -1 #not found
 -2 #confirmation error
 -3 #already registered 
--4 #command not found
+-4 #command doesn't exist
 -5 #not loged in
 -6 #use database first
+-7 #repeated entry
+-8 #non-nullable entry missing
+-9 #type not matching
+-10 #repeated primary key value 
+-11 #wrong column name
+-12 #this is reserved word
+-13 #too many args
+-14 #command denied 
+-15 #wrong value
